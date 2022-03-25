ed1d4dc11083ea26fcff4675cf2f94b9f15941a7
maXimalist <www.maximalist@mail.ru>
2022-03-25 16:53:59 +0300

    virtual-dom/step-2

--- samples/App.jsx
+++ samples/App.jsx
@@ -1,31 +1,8 @@
 import OwnReact from "../src";
 import List from "./List/List";
-import { changeArrayItemsOrder } from "../src/service/helpers";
 
-
-const LETTERS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-
-class App extends OwnReact.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lettersList: LETTERS.split("")
-    };
-    this.handleChangeLettersOrder();
-  }
-
-  handleChangeLettersOrder() {
-    setInterval(() => {
-      this.setState({
-        lettersList: changeArrayItemsOrder(this.state.lettersList)
-      });
-    }, 1000);
-  }
-
-  render() {
-    const { lettersList } = this.state;
-    return <List letters={lettersList} />;
-  }
-}
+const App = () => {
+  return <List />;
+};
 
 export default App;
